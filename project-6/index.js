db9739d132e0f1c1c208245c4c2217295ba49df6
AbhipshaChandel <chandelabhipsha0007@gmail.com>
2026-04-02 23:47:54 +0530

    Color the boxes using JS

---
+++ project-6/index.js
@@ -0,0 +1,13 @@
+function randomcolor(){
+    let x=Math.floor(Math.random()*256)
+    let y=Math.floor(Math.random()*256)
+    let z=Math.floor(Math.random()*256)
+    return `rgb(${x},${y},${z})`
+}
+
+
+setInterval(()=>{
+    document.querySelectorAll(".box").forEach((x)=>{
+      x.style.backgroundColor=randomcolor()
+    })
+},1000)
