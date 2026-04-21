2ba8176746e40f987d87d4856b055a115c31fe7d
AbhipshaChandel <chandelabhipsha0007@gmail.com>
2026-04-21 10:53:45 +0530

    updated script

--- project-2/script.js
+++ project-2/script.js
@@ -42,8 +42,10 @@
 let saveBtn=document.getElementById("saveBtn")
 let list=document.getElementById("bookmarkList")
 
-window.onload=function(){
    let bookmarks=JSON.parse(localStorage.getItem("bookmarks")) || []
+
+     function showbookmark(){
+      list.innerHTML=""
    bookmarks.forEach(bookmark => {
     addBookmarkToPage(bookmark.name,bookmark.url)
    });
@@ -61,23 +63,42 @@ window.onload=function(){
 
    addBookmarkToPage(name,url)
 
-   let bookmarks=JSON.parse(localStorage.getItem("bookmarks")) || []
    bookmarks.push({name,url})
-   localStorage.setItem("bookmarks",JSON.stringify(bookmarks))
+   updateStorage()
+   showbookmark()
 
    document.getElementById("siteName").value=""
    document.getElementById("siteURL").value=""
 })
 
-function addBookmarkToPage(name,url){
+
+
+function addBookmarkToPage(name,url,index){
     let li=document.createElement("li")
     let a=document.createElement("a")
+    let delbtn=document.createElement("button")
+    delbtn.innerHTML="X"
+
+    delbtn.addEventListener("click",(e)=>{
+      e.stopPropagation()
+      bookmarks.splice(index,1)
+      updateStorage()
+      showbookmark()
+    })
 
     a.href=url
     a.innerText=name
     a.target="_blank"
-
+    li.appendChild(delbtn)
     li.appendChild(a)
     list.appendChild(li)
 
 }
+
+function updateStorage(){
+   localStorage.setItem("bookmarks",JSON.stringify(bookmarks))
+
+}
+
+window.onload=showbookmark()
+
